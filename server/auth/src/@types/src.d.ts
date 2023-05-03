d088fff23f4d988cf71216341906ae25a7248c3a
whybe7 <whybe7@hotmail.com>
2023-05-03 20:56:57 +0100

    Separate the leorodney to server and client repos

--- server/auth/src/@types/src.d.ts
+++
@@ -1,8 +0,0 @@
-// declare module for src folder tjat 
-declare module 'src' {
-    const src: {
-        (path: string): Promise<boolean>;
-        sync(path: string): boolean;
-    };
-    export = src;
-}

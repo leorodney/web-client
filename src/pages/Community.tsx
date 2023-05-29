7b7998a12cb69f521ed0fc7b35ff51fefbdddd7e
whybe7 <whybe7@hotmail.com>
2023-05-29 23:07:20 +0100

    adding a header in the community page

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -2,11 +2,13 @@ import Hero from "../components/Hero";
 import ShowCases from "../components/ShowCases";
 import useAuthorization from "../hooks/authorization";
 import Production from "../components/Production";
+import Header from "../components/Header";
 
 export default function Community() {
   useAuthorization("/login");
   return (
     <>
+      <Header/>
       <Hero/>
       <Production/>
       <ShowCases/>

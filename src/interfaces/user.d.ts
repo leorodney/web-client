ccd2e9f99fe4b8366735645f0cbc3bb3f49d492a
whybe7 <whybe7@hotmail.com>
2023-05-31 00:26:13 +0100

    updating the user type to holde email, profile pic, join date with UserProfile interface

--- src/interfaces/user.d.ts
+++ src/interfaces/user.d.ts
@@ -1,5 +1,14 @@
 export interface AuthUser {
     isAuthenticated: boolean = false;
+    email: string = "";
     username: string = "";
     uid: string = "";
+    profilePicture: string = "";
+    joined?: Date = new Date();
+}
+
+export interface UserProfile{
+    updateable: boolean = false;
+    user: AuthUser;
+    
 }

890c0704d79ac9dfa2fd9ca68b671e1cd0eae0b3
whybe7 <whybe7@hotmail.com>
2023-05-30 18:37:38 +0100

    adding the missing 404 not found handler page with route configuration

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+
+export default function NotFound() {
+  return (
+    // return a 404 not found page
+    <section className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-9xl font-bold">404</h1>
+        <h2 className="text-2xl font-bold">Page Not Found</h2>
+    </section>
+  )
+}

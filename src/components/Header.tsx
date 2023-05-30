71a8b8abc3d297c1197c34eb137c28c44e207f43
whybe7 <whybe7@hotmail.com>
2023-05-31 00:17:16 +0100

    drop down header menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,23 @@
-import leorodney from '../assets/leorodney-w.png';
+import { Link } from 'react-router-dom';
+import defaultUser from "../assets/default-user.jpg";
+import { useSelector } from 'react-redux';
+import { StoreState } from '../interfaces/store';
+import Logout from './Logout';
 
 export default function Header() {
+  const {username, profilePicture} = useSelector((state: StoreState) => state.user);
+  console.log(profilePicture)
   return (
-    <header className="flex items-center justify-center gap-10 flex-col">
-        <img src={leorodney} className='w-48' alt="leorodney logo" />
-        <h1 className='text-white text-8xl font-thin font-serif' >LEORDONEY</h1>
-        <p className='text-gray-200 text-xl'>AI image generator based on Dall-E OpenAI</p>
+    <header className="absolute h-16 w-full top-0 z-[999] flex items-center justify-center gap-10 flex-col p-2">
+        {/* navbar with login,register  and profile picture if the user logged in shows the logout button */}
+        <nav className="absolute right-8 flex group items-center flex-row-reverse w-10 h-10 rounded-full bg-slate-700 bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${profilePicture || defaultUser})`}}>
+          {/* a profile picture circle when hover over it displays a dropdown menu  */}
+          <ul className='absolute w-32 top-12 flex flex-col rounded-md bg-white overflow-hidden'>
+            <li className="py-2 px-2 flex items-center justify-evenly w-full text-gray-400 font-medium hover:cursor-pointer hover:text-[var(--clr-p)] transition-all hover:bg-gray-200"><Link to={`/${username}`}>{username}</Link></li>
+            <li className='py-2 px-2 flex items-center justify-evenly w-full transition-all hover:bg-gray-200'><Link to={"/about"}>About</Link></li>
+            <li className='py-2 px-2 text-center transition-all hover:bg-red-100'><Logout /></li>
+          </ul>
+        </nav>
     </header>
   )
 }
